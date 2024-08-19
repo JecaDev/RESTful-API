36cdfd7734269c9cc13d83c1e7ec5882c4755070
jeanm <jeanmacedo1302@gmail.com>
2024-08-19 16:59:25 -0300

    Initial commit

---
+++ src/test/java/jean/example/RESTful/API/ResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jean.example.RESTful.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
